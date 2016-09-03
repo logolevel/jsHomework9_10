0fcb327511cc5243b6c8dec491b0f06b35ce6313
Danil Zaviryukha <logolevel@gmail.com>
2016-09-04 00:01:18 +0300

    Added carousel

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function() {
+
+//carousel
+ $("#owl-demo").owlCarousel({
+
+  autoPlay: 5000,
+  items : 2,
+  navigation: true,
+  navigationText: ['<', '>'],
+  responsive: false
+
+ });
+
+});
